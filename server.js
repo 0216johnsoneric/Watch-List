68ba32e007393b7909e37fb2f9f45b859943b750
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-26 23:58:36 -0400

    Done.

--- server.js
+++ server.js
@@ -17,9 +17,43 @@ var connection = mysql.createConnection({
   port: 3306,
   user: "root",
   password: "password",
-  database: "watch_list_db"
+  database: "movies_db"
 });
 
+connection.connect(function(err) {
+  if (err) {
+    console.error("error connecting: " + err.stack);
+    return;
+  }
 
+  console.log("connected as id " + connection.threadId);
+});
  
-connection.connect();
+// Use Handlebars to render the main index.html page with the plans in it.
+app.get("/", function(req, res) {
+  connection.query("SELECT * FROM movies_db;", function(err, data) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    res.render("index", { movies_db: data });
+  });
+});
+
+app.post("/movies", function(req, res) {
+  connection.query("INSERT INTO movies_db (movie) VALUES (?)", [req.body.movie], function(err, result) {
+    if (err) {
+      return res.status(500).end();
+    }
+
+    // Send back the ID of the new plan
+    res.json({ id: result.insertId });
+    console.log({ id: result.insertId });
+  });
+});
+
+// Start our server so that it can begin listening to client requests.
+app.listen(PORT, function() {
+  // Log (server-side) when our server has started
+  console.log("Server listening on: http://localhost:" + PORT);
+});
