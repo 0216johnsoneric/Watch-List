a5e25462ba676ea199e7576ca534114b56387c99
Eric Johnson <ericjohnson0216@icloud.com>
2020-10-26 21:41:30 -0400

    Done.

---
+++ server.js
@@ -0,0 +1,25 @@
+var express = require("express");
+var exphbs = require("express-handlebars");
+var mysql = require('mysql');
+
+var app = express();
+var PORT = process.env.PORT || 8080;
+
+// Sets up the Express app to handle data parsing
+app.use(express.urlencoded({ extended: true }));
+app.use(express.json());
+
+app.engine("handlebars", exphbs({ defaultLayout: "main" }));
+app.set("view engine", "handlebars");
+
+var connection = mysql.createConnection({
+  host: "localhost",
+  port: 3306,
+  user: "root",
+  password: "password",
+  database: "watch_list_db"
+});
+
+
+ 
+connection.connect();
